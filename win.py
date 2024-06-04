8bacbd74d272fae26a38e2203b6c056f55ba125a
Charlie Jarl <charlie.jarl@gmail.com>
2024-06-04 14:58:02 +0200

    custom_tk

--- win.py
+++ win.py
@@ -1,4 +1,3 @@
-#import tkinter as tk
 import customtkinter as ctk
 
 def click():
@@ -8,8 +7,6 @@ win = ctk.CTk()
 ctk.set_appearance_mode("light")
 ctk.set_default_color_theme("green")
 win.title("Python GUI")
-#win.geometry("300x200")
-#win.config(background="lightblue")
 lable = ctk.CTkLabel(win, text="Hello World", font=("Arial Bold", 20))
 lable.pack(padx=50, pady=20)
 button = ctk.CTkButton(win, font=("Arial Bold", 24), text="NO!", command=click)
